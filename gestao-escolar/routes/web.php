4e418b6b4131c25dfe31d6d279857a32aa43bcba
eveliseoliveira <98969577+eveliseoliveira@users.noreply.github.com>
2025-02-15 12:12:34 -0300

    Cria rotas

--- gestao-escolar/routes/web.php
+++ gestao-escolar/routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TurmaController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\PerfilController;
+use App\Http\Controllers\EstudanteController;
+use App\Http\Controllers\ProfessorController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('turmas', TurmaController::class);
+
+Route::resource('cursos', CursoController::class);
+
+Route::resource('professores', ProfessorController::class);
+
+Route::resource('estudantes', EstudanteController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/perfil', [PerfilController::class, 'edit'])->name('perfil.editar');
+    Route::patch('/perfil', [PerfilController::class, 'update'])->name('perfil.atualizar');
+    Route::delete('/perfil', [PerfilController::class, 'destroy'])->name('perfil.deletar');
 });
